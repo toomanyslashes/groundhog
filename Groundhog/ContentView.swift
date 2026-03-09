01db30ce16c93e2a94cf25cb7848e13f27929ebd
Haonan Liang <jason.hn.liang@gmail.com>
2026-03-09 21:27:57 +0800

    Initial Commit

---
+++ Groundhog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Groundhog
+//
+//  Created by LIANG Haonan  on 2026/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Groundhog/GroundhogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundhogApp.swift
+//  Groundhog
+//
+//  Created by LIANG Haonan  on 2026/3/9.
+//
+
+import SwiftUI
+
+@main
+struct GroundhogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
